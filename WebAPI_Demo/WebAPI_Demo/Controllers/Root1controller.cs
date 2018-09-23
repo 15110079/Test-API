16b46e121acc93143f6c7e275ffd9f776fa7f11c
Minh Huynh Kien <minhhuynh1703@gmail.com>
2018-09-23 19:05:07 +0700

    merge root

---
+++ WebAPI_Demo/WebAPI_Demo/Controllers/Root1controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPI_Demo.Controllers
+{
+    public class Root1controller : Controller
+    {
+        // GET: Root1controller
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAPI_Demo/WebAPI_Demo/Controllers/Root2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPI_Demo.Controllers
+{
+    public class Root2Controller : Controller
+    {
+        // GET: Root2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
